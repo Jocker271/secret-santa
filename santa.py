74b59bb9106f1c1074f628fdc38495c2b925b352
Wehden, Johannes (Intero Technologies) <j.wehden@intero-technologies.de>
2022-11-07 09:22:13 +0100

    [WIP] Varaible Naming

--- santa.py
+++ santa.py
@@ -1,24 +1,37 @@
-import os
+"""
+Created on 2022-11-01
+by Johanes Wehden (Jocker271)
+"""
+
+import os, random
 import pandas as pd
 
-def get_persons_dataframe():
+def get_santa_dataframe():
     """read persons.csv and convert the data in a DataFrame"""
     file_path = f'{os.path.dirname(__file__)}\\persons.csv'
-    df = pd.read_csv(file_path)
-    df.columns = [col.upper() for col in df.columns]
-    df = df.convert_dtypes()
-    if 'ID' in df and df['ID'].is_unique:
-        if "NOT MATCHING" in df:
-            df['NOT MATCHING'] = df['NOT MATCHING'].fillna("").map(
-                lambda x: x.split(' '))
-        return df
+    santa_df = pd.read_csv(file_path).convert_dtypes()
+    santa_df.columns = [col.upper() for col in santa_df.columns]
+    if 'ID' in santa_df and santa_df['ID'].is_unique:
+        santa_df.set_index(['ID'], inplace=True)
+        if "NOT MATCHING" in santa_df:
+            santa_df['NOT MATCHING'] = santa_df['NOT MATCHING'].fillna("").map(
+                lambda ids: ids.split(' '))
+        return santa_df
     else:
         return False
 
+def create_santa_matches(santa_df):
+    for santa in len(santa_df):
+        get_match()
+    return True
+
+def get_match(santa_df):
+    return True
 
 def main():
-    df = get_persons_dataframe()
-    print(df)
+    santa_df = get_santa_dataframe()
+    test = create_santa_matches(santa_df)
+    print(santa_df)
 
 if __name__ == "__main__":
     main()
